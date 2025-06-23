d2b84c62ca8225ac3ba538029723944303fa6170
RakibMozumder <34626905+RakibMozumder@users.noreply.github.com>
2025-06-23 23:54:06 +0600

    vercel deployment minor fix

--- dist/api/index.js
+++ dist/api/index.js
@@ -4,4 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("../app"));
-exports.default = app_1.default;
+exports.default = (req, res) => {
+    (0, app_1.default)(req, res);
+};
